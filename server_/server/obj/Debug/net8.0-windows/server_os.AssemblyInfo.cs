adb37914750cd9b617806b6fb5dab8adf710ef51
Minh-Duc-1606 <tranleminhduc7796@gmail.com>
2025-12-16 21:27:05 +0700

    update server UI

--- server_/server/obj/Debug/net8.0-windows/server_os.AssemblyInfo.cs
+++ server_/server/obj/Debug/net8.0-windows/server_os.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server_os")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83254d1095b553291f2f69da77538726cae1cb45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425b9c15abe28d53cbe8d0a89286b81ae010f8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("server_os")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server_os")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
